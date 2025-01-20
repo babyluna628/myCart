55a40ffdd061b0a382cfdb5eea09f45bc4b9f55d
dh <dohyun134@naver.com>
2025-01-20 12:51:11 +0900

    싱글프로덕트

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import HomePage from "./components/Home/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import ProductsPage from "./components/Products/ProductsPage";
+import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       {/* <HomePage /> */}
-      <ProductsPage />
+      {/* <ProductsPage /> */}
+      <SingleProductPage />
     </div>
   );
 }
