bf0d71cc49c58f3dca34f84d9f54d0f78f046e92
dh <dohyun134@naver.com>
2025-01-21 16:10:53 +0900

    커스텀훅

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,19 +1,35 @@
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import "./ProductsSidebar.css";
-import rocket from "../../assets/rocket.png";
+import { useEffect } from "react";
+import apiClient from "../../utils/api-client";
+import useData from "../../Hook/useData";
 
 const ProductsSidebar = () => {
+  const { data: categories, error } = useData("category");
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      //.then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
   return (
     <aside className="products_sidebar">
       <h2>카테고리</h2>
 
       <div className="category_links">
-        <LinkWithIcon
-          title="전자제품"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              title={category.name}
+              link={`/products?category=${category.name}`}
+              emoji={`http://localhost:5000/category/${category.image}`}
+              sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
