2906e9305ee23312b93271ce4811210e159df4cc
dh <dohyun134@naver.com>
2025-01-23 15:04:27 +0900

    토큰을axios헤더에 저장

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+
+//만약 토큰이 있으면 axios 설정에 추가됨
+setAuthToken(localStorage.getItem("token"));
 
 function App() {
   const [user, setUser] = useState(null);
   const [cart, setCart] = useState([]); //장바구니 물건 갯수
 
   const addToCart = (product, quantity) => {
-    setCart([...cart, { product, quantity }]);
+    const updatedCart = [...cart]; //장바구니 복사
+    const productIndex = updatedCart.findIndex(
+      (item) => item.product._id === product._id
+    );
+    if (productIndex === -1) {
+      updatedCart.push({ product: product, quantity: quantity });
+    } else {
+      updatedCart[productIndex].quantity += quantity;
+    }
+    setCart(updatedCart);
   };
 
   useEffect(() => {
