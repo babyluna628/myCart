82d747dc2a3b1f07939df9d59f926dcb27db0d04
dh <dohyun134@naver.com>
2025-01-24 09:31:44 +0900

    카트정보를 카트페이지로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { addToCartAPI } from "./services/cartServices";
+import { addToCartAPI, getCartAPI } from "./services/cartServices";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -36,6 +36,20 @@ function App() {
       });
   };
 
+  //서버에서 장바구니 정보 가져옴
+  const getCart = () => {
+    getCartAPI()
+      .then((res) => {
+        setCart(res.data);
+      })
+      .catch((err) => {
+        toast.error("카트 가져오기를 실패했습니다.");
+      });
+  };
+  useEffect(() => {
+    if (user) getCart();
+  }, [user]);
+
   useEffect(() => {
     try {
       const jwt = localStorage.getItem("token");
@@ -53,12 +67,8 @@ function App() {
     <div className="app">
       <Navbar user={user} cartCount={cart.length} />
       <main>
-        <ToastContainer
-          position="bottom=right"
-          autoClose={3000}
-          closeOnClick={true}
-        />
-        <Routing addToCart={addToCart} />
+        <ToastContainer />
+        <Routing addToCart={addToCart} cart={cart} />
       </main>
     </div>
   );
